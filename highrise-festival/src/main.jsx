aa8c3584baf4d1318f7586b6583fadbb3bc78ceb
Flynn Stacey <flynnstacey@hotmail.com>
2023-04-20 21:36:00 +0100

    added vercel analytics

--- highrise-festival/src/main.jsx
+++ highrise-festival/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App";
 import ScrollToTop from "./components/helpers/ScrollToTop/ScrollToTop";
 import "./index.css";
@@ -9,6 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <ScrollToTop />
+      <Analytics />
       <App />
     </BrowserRouter>
   </React.StrictMode>
